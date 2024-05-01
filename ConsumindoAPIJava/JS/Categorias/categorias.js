d40d4f274b798ff425329d2ce56606bcb8068ec6
unknown <orlyjunior21@gmail.com>
2024-05-01 00:15:19 -0300

    Estilização do site que consome a API Java e início do fetch do POST de categoria.

--- ConsumindoAPIJava/JS/Categorias/categorias.js
+++ ConsumindoAPIJava/JS/Categorias/categorias.js
@@ -1,3 +1,15 @@
 function consultar(){
     window.location.href = "consultarCategorias.html"
 }
+
+function adicionar(){
+    window.location.href = "adicionarCategorias.html"
+}
+
+function deletar(){
+    window.location.href = "deletarCategorias.html"
+}
+
+function editar(){
+    window.location.href = "editarCategorias.html"
+}
